b38c4b7f5106d246314a064064c703c85407f2fc
shiwei <yulaie1012@gmail.com>
2022-08-30 06:12:44 +0800

    Link pages

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "blog"
 urlpatterns = [
     path("", views.index, name="index-page"),
-    path("posts/", views.post_list, name="posts-page"),
+    path("posts/", views.post_list, name="post-list-page"),
     path("posts/<slug:slug>/", views.post_detail, name="post-detail-page"),
 ]
