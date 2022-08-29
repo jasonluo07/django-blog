03815cd7121d3cd6faca4a921f161f5a40151cc6
shiwei <yulaie1012@gmail.com>
2022-08-30 07:35:57 +0800

    Add template: 404

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = "django-insecure-h@=ujg02j$z8#334l@z-c*ptj@dq(c(+k^@35h26mz(g8=eln8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
