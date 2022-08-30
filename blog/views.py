1478822c8756c99a7367fc559518de3f3d2a9c73
shiwei <yulaie1012@gmail.com>
2022-08-30 09:49:03 +0800

    Update views with models

--- blog/views.py
+++ blog/views.py
@@ -1,75 +1,10 @@
-from datetime import date
+from django.shortcuts import get_object_or_404, render
 
-from django.shortcuts import render
-
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Maximilian",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """,
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """,
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """,
-    },
-]
-
-
-def get_date(entity):
-    return entity["date"]
+from .models import Post
 
 
 def index(request):
-    sorted_posts = sorted(all_posts, key=get_date, reverse=True)
-    latest_posts = sorted_posts[:3]
+    latest_posts = Post.objects.order_by("-created_time")[:3]
     context = {
         "posts": latest_posts,
     }
@@ -78,6 +13,7 @@ def index(request):
 
 
 def post_list(request):
+    all_posts = Post.objects.order_by("-created_time")
     context = {
         "posts": all_posts,
     }
@@ -86,9 +22,9 @@ def post_list(request):
 
 
 def post_detail(request, slug):
-    post = next(post for post in all_posts if post["slug"] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     context = {
-        "post": post,
+        "post": identified_post,
     }
 
     return render(request, "blog/post-detail.html", context)
