d7eb350115b1767a18a4989d42ef5700ea1ad728
shiwei <yulaie1012@gmail.com>
2022-08-30 11:54:53 +0800

    Refactor url names

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from .views import IndexView, PostDetailView, PostListView
 
 app_name = "blog"
 urlpatterns = [
-    path("", IndexView.as_view(), name="index-page"),
-    path("posts/", PostListView.as_view(), name="post-list-page"),
-    path("posts/<slug:slug>/", PostDetailView.as_view(), name="post-detail-page"),
+    path("", IndexView.as_view(), name="index"),
+    path("posts/", PostListView.as_view(), name="post-list"),
+    path("posts/<slug:slug>/", PostDetailView.as_view(), name="post-detail"),
 ]
