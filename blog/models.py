6ab845cf75e4311e11917deb2fdcde8f4fcce07d
shiwei <yulaie1012@gmail.com>
2022-08-30 11:33:20 +0800

    Use ImageField

--- blog/models.py
+++ blog/models.py
@@ -26,7 +26,7 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     slug = models.SlugField(unique=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     created_time = models.DateField(auto_now_add=True)
